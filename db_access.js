1600035553e9702cb239dbf81b3a24671c660cba
Anna Kostrytsa <annakostrytsa@Annas-MacBook-Pro.local>
2024-02-27 14:17:52 +0100

    receiving data from DB

--- db_access.js
+++ db_access.js
@@ -1,19 +1,47 @@
 var id = "1Kbu0D1wolVaW5bAxslj9mSDvMUgOtVNZTctC2qp2Ssw";
 var gid = "0";
-var front_data;
+let front_data;
+let img_arr = [];
+let counter = 0;
 
-var url =
-  "https://docs.google.com/spreadsheets/d/" +
-  id +
-  "/gviz/tq?tqx=out:json&tq&gid=" +
-  gid;
-fetch(url)
-  .then((response) => response.text())
-  .then((data) => {
-    front_data = JSON.parse(data.substring(47).slice(0, -2));
-    console.log(data.substring(47).slice(0, -2));
-    console.log(front_data);
-  });
+function redirectToSliderPage() {
+  // Redirect to the slider page
+  window.location.href = "slider_page.html";
+}
 
-console.log(front_data);
-console.log("trololololo");
+// Data from Google Sheets
+async function fetchData() {
+  const id = "1Kbu0D1wolVaW5bAxslj9mSDvMUgOtVNZTctC2qp2Ssw";
+  const gid = "0";
+  const url = `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:json&tq&gid=${gid}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    const jsonData = JSON.parse(data.substring(47).slice(0, -2));
+    return jsonData;
+  } catch (error) {
+    console.error("Error catch:", error);
+    return null;
+  }
+}
+
+fetchData().then((jsonData) => {
+  if (jsonData) {
+    console.log("Data:", jsonData.table.rows);
+
+    jsonData.table.rows.forEach((element, i) => {
+      counter++;
+      img_arr[i] = {
+        original_img: element.c[0].v,
+        description: element.c[1].v,
+        generated_img: element.c[2].v,
+      };
+    });
+
+    console.log(img_arr);
+    console.log(counter);
+  } else {
+    console.log("Error!");
+  }
+});
--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-function redirectToSliderPage() {
-  // Redirect to the slider page
-  window.location.href = "slider_page.html"; // Change "slider_page.html" to the actual URL of your slider page
-}
-
 // Container
 let container = document.querySelector("#container");
 
@@ -27,24 +22,25 @@ container.addEventListener("touchstart", touchStartFn);
 container.addEventListener("touchmove", touchMoveFn);
 
 function touchStartFn(event) {
-    touchStartX = event.touches[0].clientX;
+  touchStartX = event.touches[0].clientX;
 }
 
 function touchMoveFn(event) {
-    touchEndX = event.touches[0].clientX;
-    const swipeDistance = touchEndX - touchStartX;
-    const direction = swipeDistance > 0 ? 1 : -1; // 1 for right swipe, -1 for left swipe
-    const sectionNumber = findImageSection(touchEndX, screenSection);
-    const newImgIndex = (sectionNumber - 1 + direction + numOfImages) % numOfImages + 1;
-    img.src = `img/${newImgIndex}.JPG`;
+  touchEndX = event.touches[0].clientX;
+  const swipeDistance = touchEndX - touchStartX;
+  const direction = swipeDistance > 0 ? 1 : -1; // 1 for right swipe, -1 for left swipe
+  const sectionNumber = findImageSection(touchEndX, screenSection);
+  const newImgIndex =
+    ((sectionNumber - 1 + direction + numOfImages) % numOfImages) + 1;
+  img.src = `img/${newImgIndex}.JPG`;
 }
 
 // Function to find the section of the image
 function findImageSection(touchEndX, screenSection) {
-    for (let i = 1; i <= numOfImages; i++) {
-        if (touchEndX < screenSection * i) {
-            return i;
-        }
+  for (let i = 1; i <= numOfImages; i++) {
+    if (touchEndX < screenSection * i) {
+      return i;
     }
-    return numOfImages; // If the touch end position is beyond the last section, return the last section
+  }
+  return numOfImages; // If the touch end position is beyond the last section, return the last section
 }
