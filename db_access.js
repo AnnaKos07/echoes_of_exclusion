88a391fee78a8031d64e78ecf1bf63454c7bd1ca
Anna Kostrytsa <annakostrytsa@Annas-MacBook-Pro.local>
2024-02-28 16:23:58 +0100

    rebasing of db data fetch

--- db_access.js
+++ db_access.js
@@ -1,73 +1,51 @@
-// const express = require("express");
-// const fetch = require("node-fetch");
+// let counter = 0;
+// let img_arr = [];
+// async function fetchData() {
+//   //const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+//   const id = "1Kbu0D1wolVaW5bAxslj9mSDvMUgOtVNZTctC2qp2Ssw";
+//   const gid = "0";
+//   const url = `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:json&tq&gid=${gid}`;
 
-// const app = express();
-// const port = 3000;
-
-// const id = "1Kbu0D1wolVaW5bAxslj9mSDvMUgOtVNZTctC2qp2Ssw";
-// const gid = "0";
-// const url = `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:json&tq&gid=${gid}`;
-
-// app.use(express.json());
-
-// app.get("/data", async (req, res) => {
 //   try {
-//     const response = await fetch(url); // Замените на ваш URL
-//     const data = await response.json();
-//     res.send(data); // Отправляем данные как ответ
-//     console.log(data);
+//     const response = await fetch(url, { mode: "cors" });
+//     const data = await response.text();
+//     console.log(response);
+//     const jsonData = JSON.parse(data.substring(47).slice(0, -2));
+//     console.log("trolololoo");
+//     console.log(jsonData);
+//     return jsonData;
 //   } catch (error) {
-//     console.error("Ошибка:", error);
-//     res.status(500).json({ error: "Ошибка при получении данных" });
+//     console.error("Error catch:", error);
+//     return null;
 //   }
-// });
+// }
 
-// app.listen(port, () => {
-//   console.log(`Прокси-сервер запущен на порту ${port}`);
-// });
+// function redirectToSliderPage() {
+//   fetchData().then((jsonData) => {
+//     if (jsonData) {
+//       console.log("Data:", jsonData.table.rows);
 
-async function fetchData() {
-  //const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  const id = "1Kbu0D1wolVaW5bAxslj9mSDvMUgOtVNZTctC2qp2Ssw";
-  const gid = "0";
-  const url = `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:json&tq&gid=${gid}`;
+//       jsonData.table.rows.forEach((element, i) => {
+//         counter++;
 
-  try {
-    const response = await fetch(url, { mode: "cors", method: "GET" });
-    const data = await response.text();
-    console.log(response);
-    const jsonData = JSON.parse(data.substring(47).slice(0, -2));
-    console.log("trolololoo");
-    console.log(jsonData);
-    return jsonData;
-  } catch (error) {
-    console.error("Error catch:", error);
-    return null;
-  }
-}
+//         img_arr[i] = {
+//           original_img: element.c[0].v,
+//           description: element.c[1].v,
+//           generated_img: element.c[2].v,
+//         };
+//         console.log(img_arr[i]);
+//       });
+
+//       console.log(img_arr);
+//       console.log(counter);
+//       const queryParams = encodeURIComponent(JSON.stringify(img_arr));
+//       window.location.href = `another_slider.html?img_arr=${queryParams}`;
+//     } else {
+//       console.log("Error!");
+//     }
+//   });
+// }
 
 function redirectToSliderPage() {
-  fetchData().then((jsonData) => {
-    if (jsonData) {
-      console.log("Data:", jsonData.table.rows);
-
-      jsonData.table.rows.forEach((element, i) => {
-        counter++;
-
-        img_arr[i] = {
-          original_img: element.c[0].v,
-          description: element.c[1].v,
-          generated_img: element.c[2].v,
-        };
-        console.log(img_arr[i]);
-      });
-
-      console.log(img_arr);
-      console.log(counter);
-      const queryParams = encodeURIComponent(JSON.stringify(img_arr));
-      window.location.href = `another_slider.html?img_arr=${queryParams}`;
-    } else {
-      console.log("Error!");
-    }
-  });
+  window.location.href = `another_slider.html`;
 }
--- slider.js
+++ slider.js
@@ -1,24 +1,50 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Получение параметров URL
-  const urlParams = new URLSearchParams(window.location.search);
-  const imgArrParam = urlParams.get("img_arr");
+let counter = 0;
+let img_arr = [];
+async function fetchData() {
+  //const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const id = "1Kbu0D1wolVaW5bAxslj9mSDvMUgOtVNZTctC2qp2Ssw";
+  const gid = "0";
+  const url = `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:json&tq&gid=${gid}`;
 
-  if (imgArrParam) {
-    const imgArr = JSON.parse(decodeURIComponent(imgArrParam));
-    createSlider(imgArr);
+  try {
+    const response = await fetch(url, { mode: "cors" });
+    const data = await response.text();
+    const jsonData = JSON.parse(data.substring(47).slice(0, -2));
+    return jsonData;
+  } catch (error) {
+    console.error("Error catch:", error);
+    return null;
   }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchData().then((jsonData) => {
+    if (jsonData) {
+      console.log("Data:", jsonData.table.rows);
+
+      jsonData.table.rows.forEach((element, i) => {
+        counter++;
+
+        img_arr[i] = {
+          original_img: element.c[0].v,
+          description: element.c[1].v,
+          generated_img: element.c[2].v,
+        };
+      });
+      createSlider(img_arr);
+    } else {
+      console.log("Error!");
+    }
+  });
 });
 
 function createSlider(imgArr) {
-  // Создание элемента слайдера (например, с использованием библиотеки Swiper)
   const swiperContainer = document.getElementById("swiper-container");
   const swiperWrapper = document.createElement("div");
   swiperWrapper.classList.add("swiper-wrapper");
   swiperContainer.appendChild(swiperWrapper);
 
   var mySwiper = new Swiper(".swiper-container", {
-    // Параметры Swiper
-    loop: true, // Бесконечный цикл
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
